161de0272e95fafc1300668924ab8bddd9130cd4
Rennan Pessanha <94031223+Rennan-Pessanha@users.noreply.github.com>
2023-06-27 01:14:32 -0300

    Perfil da Empresa criado

--- src/Pages/Perfil/index.js
+++ src/Pages/Perfil/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import * as C from './styles.js'
+import Footer from '../../Components/Footer/index.js'
 
 export const Perfil = () => {
   return (
@@ -58,7 +59,47 @@ export const Perfil = () => {
         </C.CompanyContent>
 
       </C.Container>
-    
+      <C.FooterPerfil>
+        <C.FooterContentWrapper>
+      <C.PerfisWrapper>
+        <C.PerfisContents>
+            <C.PerfilImage src="./Imagem/Icons/Group.svg" alt="Imagem do perfil" />
+            <C.PerfilContentTitle>Atendimento</C.PerfilContentTitle>
+            <C.PerfilDescription>Relacionamento com o cliente é o nosso principal pilar.</C.PerfilDescription>
+        </C.PerfisContents>
+          <C.PerfisContents>
+            <C.PerfilImage src="./Imagem/Icons/Estrela.svg" alt="Imagem do perfil" />
+            <C.PerfilContentTitle>Qualidade</C.PerfilContentTitle>
+            <C.PerfilDescription>Silaços é reconhecida por sua agilidade na entrega de produtos sob medida.</C.PerfilDescription>
+        </C.PerfisContents>
+          <C.PerfisContents>
+            <C.PerfilImage src="./Imagem/Icons/Pagamento.svg" alt="Imagem do perfil" />
+            <C.PerfilContentTitle>Pagamento</C.PerfilContentTitle>
+            <C.PerfilDescription>Aceitamos PIX, Cartão de crédito e Faturamento.</C.PerfilDescription>
+        </C.PerfisContents>
+        </C.PerfisWrapper>
+      <C.ContentF>
+        <C.LogoContent>
+            <C.LogoF src="./Imagem/LogoSerralheria.png" alt="Logo da Empresa" />
+        </C.LogoContent>
+    <C.CompanyFContent>
+        <C.CompanyTitle>Sobre a Empresa</C.CompanyTitle>
+        <C.CompanyDescription>
+        A Silaços trabalha com os principais fabricantes e distribuidores de produtos para estrutura metálica do mercado, oferecendo diversas opções para atender prontamente às suas necessidades. Com isso, podemos otimizar o seu tempo e fornecer acesso aos melhores valores do mercado.
+         </C.CompanyDescription>
+    </C.CompanyFContent>
+    <C.ContactContent>
+        <C.ContactTitle>Contato</C.ContactTitle>
+        <C.ContactList>
+            <C.ContactListItem>Whatsapp:</C.ContactListItem>
+            <C.ContactListItem>Email:</C.ContactListItem>
+            <C.ContactListItem>Telefone:</C.ContactListItem>
+            <C.ContactListItem>Fale Conosco:</C.ContactListItem>
+        </C.ContactList>
+    </C.ContactContent>
+      </C.ContentF>
+      </C.FooterContentWrapper>
+      </C.FooterPerfil>
     </>
   )
 }
--- src/Pages/Perfil/styles.js
+++ src/Pages/Perfil/styles.js
@@ -7,8 +7,20 @@ export const Container = styled.div`
   background-size: cover;
   background-repeat: no-repeat;
 
-  @media (max-width: 768px) {
+  @media (max-width: 1440px) {
     height: 180vh;
+  }
+
+  @media (max-width: 1024px) {
+    height: 230vh;
+  }
+
+  @media (max-width: 768px) {
+    height: 240vh;
+  }
+
+  @media (max-width: 480px) {
+    height: 360vh;
   }
 `;
 
@@ -120,6 +132,12 @@ export const CompanyContent = styled.div`
     text-align: center;
     margin-top: 28%;
   }
+
+  @media (max-width: 480px) {
+    margin-left: 0;
+    text-align: center;
+    margin-top: 110%;
+  }
 `;
 
 export const History = styled.div`
@@ -134,4 +152,190 @@ export const Textp = styled.p`
   font-size: 24px;
   font-family: Montserrat;
   font-weight: 600;
+`;
+
+
+
+
+
+
+
+export const FooterPerfil = styled.div`
+  position: relative;
+  height: 100vh;
+  width: 100%;
+  background-color: #242424;
+
+  @media (max-width: 768px) {
+    height: 270vh;
+  }
+`;
+
+export const FooterContentWrapper = styled.div`
+  position: absolute;
+`;
+
+export const PerfisWrapper = styled.div`
+  display: flex;
+  margin-right: 6%;
+  margin-left: -11%;
+
+  @media (max-width: 768px) {
+    display: block;
+  }
+`;
+
+export const PerfisContents = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 4%;
+  margin-bottom: 2%;
+  flex-direction: column;
+  margin-left: 17%;
+
+  @media (max-width: 768px) {
+    display: block;
+    margin-left: 50%;
+    margin-top: 20px;
+  }
+`;
+
+export const PerfilImage = styled.img`
+  width: 94.494px;
+  height: 94.494px;
+`;
+
+export const PerfilContentTitle = styled.h3`
+  margin-top: 10px;
+  color: #FFF;
+  font-size: 24px;
+  font-family: Montserrat;
+  font-weight: 800;
+`;
+
+export const PerfilDescription = styled.p`
+  margin-top: 5px;
+  color: #FFF;
+  font-size: 17px;
+  font-family: Montserrat;
+`;
+
+
+export const ContentF = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  height: 100%;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+
+export const LogoContent = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  
+
+  @media (max-width: 768px) {
+    margin-bottom: 20px;
+  }
+`;
+
+export const LogoF = styled.img`
+  width: 475px;
+  height: 400px;
+
+  @media (max-width: 768px) {
+    width: 300px;
+    height: auto;
+    margin-left: 30%;
+  }
+`;
+
+export const CompanyFContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  flex: 1;
+`;
+
+export const CompanyTitle = styled.h3`
+  color: #fff;
+  font-size: 49px;
+  font-family: Montserrat;
+  font-weight: 800;
+  margin-left: 110px;
+
+  @media (max-width: 768px) {
+    font-size: 36px;
+    margin-left: 50px;
+  }
+`;
+
+export const CompanyDescription = styled.p`
+  font-size: 25px;
+  font-weight: 400;
+  color: #ffffff;
+  font-family: Montserrat;
+  margin-top: 8px;
+  margin-left: 75px;
+
+  @media (max-width: 768px) {
+    font-size: 20px;
+    margin-left: 50px;
+  }
+`;
+
+export const ContactContent = styled.div`
+  display: flex;
+  margin-left: 20px;
+  flex-direction: column;
+  justify-content: center;
+  flex: 1;
+
+  @media (max-width: 768px) {
+    margin-left: 0;
+  }
+`;
+
+export const ContactTitle = styled.h3`
+  color: #fff;
+  font-size: 42px;
+  font-family: Montserrat;
+  margin-left: 120px;
+  font-weight: 800;
+
+  @media (max-width: 768px) {
+    font-size: 30px;
+    margin-left: -10px;
+  }
+`;
+
+export const ContactList = styled.ul`
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  color: #fff;
+  font-size: 24px;
+  font-family: Montserrat;
+
+  @media (max-width: 768px) {
+    font-size: 18px;
+    
+  }
+`;
+
+export const ContactListItem = styled.li`
+  font-size: 16px;
+  margin-left: 155px;
+  color: #ffffff;
+  margin-bottom: 8px;
+
+  @media (max-width: 768px) {
+    font-size: 18px;
+    margin-left: 10px;
+  }
 `;
